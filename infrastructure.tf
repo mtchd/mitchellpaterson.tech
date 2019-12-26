06abce5725933adcbdfea589ec56e7f0ec9c1314
Mitchell Paterson <mitchell.paterson@rea-group.com>
2019-12-26 20:29:05 +1100

    Lambda and s3 working

--- infrastructure.tf
+++ infrastructure.tf
@@ -11,7 +11,7 @@ resource "aws_s3_bucket" "chicken-pets-highscore" {
     Name = "chicken-pets-highscore"
   }
 }
-
+// TODO: Make this role more restrictive
 resource "aws_iam_role" "iam_for_chicken" {
   name = "iam_for_chicken"
 
@@ -33,16 +33,45 @@ EOF
 }
 
 resource "aws_lambda_function" "chicken_pets_put" {
-  filename      = "put-lambda.py"
+  filename      = "put-lambda.zip"
   function_name = "chicken_pets_put"
   role          = "${aws_iam_role.iam_for_chicken.arn}"
-  handler       = "lambda_handler"
+  handler       = "put-lambda.lambda_handler"
 
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
   # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
-  source_code_hash = "${filebase64sha256("put-lambda.py")}"
+  source_code_hash = "${filebase64sha256("put-lambda.zip")}"
 
   runtime = "python3.8"
 
+}
+
+resource "aws_iam_policy" "really_insecure" {
+  name = "really_insecure"
+  path = "/"
+  description = "Really insecure policy to allow lambda, s3 and iam interactions."
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "iam:*",
+                "s3:*",
+                "lambda:*"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "test-attach" {
+  role       = "${aws_iam_role.iam_for_chicken.name}"
+  policy_arn = "${aws_iam_policy.really_insecure.arn}"
 }
--- put-lambda.py
+++ put-lambda.py
@@ -5,8 +5,8 @@ def lambda_handler(event, context):
     encoded_string = string.encode("utf-8")
 
     bucket_name = "chicken-pets-highscore"
-    file_name = "highscore.txt"
-    s3_path = "/100001/20180223/" + file_name
+    file_name = "highscore.json"
+    s3_path = file_name
 
     s3 = boto3.resource("s3")
     s3.Bucket(bucket_name).put_object(Key=s3_path, Body=encoded_string)
