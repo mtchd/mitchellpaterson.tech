3bb7fe16068d0bd10f32fdbab0979c1aa08baabe
Mitchell Paterson <mitchell.paterson@rea-group.com>
2020-01-02 20:37:06 +1100

    Scruffy fix to security

--- put-lambda.py
+++ put-lambda.py
@@ -1,17 +1,46 @@
 import boto3
+import json
 
 def lambda_handler(event, context):
 
-    print(event)
-
-    string = event["body"]
-    encoded_string = string.encode("utf-8")
-
     bucket_name = "chicken-pets-highscore"
     s3_path = "highscore.json"
+  
+    print("yeet")
 
-    s3 = boto3.resource("s3")
-    s3.Bucket(bucket_name).put_object(Key=s3_path, Body=encoded_string)
+    client = boto3.client('s3')
+
+    highscore_object = client.get_object(Bucket=bucket_name, Key=s3_path)
+
+    print(highscore_object)
+
+    highscore_raw = highscore_object["Body"].read()
+
+    highscore_json = json.loads(highscore_raw)
+
+    print(highscore_json)
+
+    highscore_string = highscore_json["highscore"]
+    
+    highscore_int = int(highscore_string)
+
+    # Now get from event
+    
+    print(event)
+
+    incoming_highscore = int(json.loads(event["body"])["highscore"])
+
+    print("Incoming Highscore:", incoming_highscore)
+
+    if (highscore_int + 1 == incoming_highscore):
+      
+      print("I'm in")
+
+      string = event["body"]
+      encoded_string = string.encode("utf-8")
+
+      s3 = boto3.resource("s3")
+      s3.Bucket(bucket_name).put_object(Key=s3_path, Body=encoded_string)
 
     return {
       "statusCode": 200,
@@ -22,5 +51,5 @@ def lambda_handler(event, context):
           "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
           "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization"
       },
-      "body": "The bucket put seems to have succeeded."
+      "body": "Yeet"
     }
