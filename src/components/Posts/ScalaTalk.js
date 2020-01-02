e52e6c4a06323f167c2dde626b069844a5c5ef16
Mitchell Paterson <mitchell.paterson@rea-group.com>
2020-01-02 12:00:11 +1100

    Update scala video link

--- src/components/Posts/ScalaTalk.js
+++ src/components/Posts/ScalaTalk.js
@@ -22,7 +22,7 @@ const ScalaTalk = ({ data }) => {
     <Col>
       <Description>
         I gave a talk running over some of the basics of Scala, feel free to check out the <a href="https://mtchd.github.io/scala-talk/#/">slides</a>, and their <a href="https://github.com/mtchd/scala-talk">source code</a>.
-        You can view the recording <a href="https://web.microsoftstream.com/video/8b164dff-83e5-4390-a218-ab0b9f30a0ad">here</a>, once it's public.
+        You can view the recording <a href="https://studio.youtube.com/video/gtcKmBvcFC0/edit">here</a>.
         The talk runs over:
       </Description>
       <TechList listItems={technologyList}/>
